d148d94b9053d2defffc688e0889f0e571be54e5
Ayberk Bey <ayberkcirak99@gmail.com>
2026-05-18 02:11:03 +0300

    hashSync basindaki await temizlendi

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ database.connect((err) => {
 //Veri ekleme
 app.post('/api/kullanici-ekle', (req, res) => {
        const { ad, soyad, eposta, telefonNumarasi, sifre } = req.body; // Verilerin değişkene atandığı yer.
-       const hashliSifre = await bcrypt.hashSync(sifre, 10);
+       const hashliSifre = bcrypt.hashSync(sifre, 10);
        const sql = 'INSERT INTO accountInformation (ad, soyad, eposta, telefonNumarasi, sifre) VALUES (?, ?, ?, ?, ?)'; // ? işaretleri güvenlik içindir.
 
        
